df183bfc66831b138c4eb05ed61a6dd202732ba8
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2026-04-01 19:56:00 +0530

    Create route.js

---
+++ app/api/projects/route.js
@@ -0,0 +1,12 @@
+import { connectDB } from "@/lib/db";
+import Project from "@/models/Project";
+
+export async function GET() {
+  await connectDB();
+  return Response.json(await Project.find());
+}
+export async function POST(req) {
+  await connectDB();
+  const data = await req.json();
+  return Response.json(await Project.create(data));
+}
