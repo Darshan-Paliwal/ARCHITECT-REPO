2f8f4f34c487d6aba56d782a5df7126f94e661cc
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2026-04-01 20:39:24 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,24 @@
+
+import { headers } from "next/headers";
+
 async function getData() {
-  const res = await fetch(process.env.NEXT_PUBLIC_URL + "/api/settings", { cache: "no-store" });
+  const host = headers().get("host");
+  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+  const res = await fetch(`${protocol}://${host}/api/settings`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) return { heroText: "Error loading" };
   return res.json();
 }
+
 export default async function Home() {
   const data = await getData();
+
   return (
     <div className="p-10">
       <h1 className="text-4xl">{data.heroText}</h1>
     </div>
   );
-}
+    }
