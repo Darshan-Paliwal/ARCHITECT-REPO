785e12e3239332079de2676a7e2aaa374538ac2c
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2026-04-01 20:31:03 +0530

    Update route.js

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,12 +1,13 @@
-import { connectDB } from "@/lib/db";
-import User from "@/models/User";
+import { connectDB } from "../../../../lib/db";
+import User from "../../../../models/User";
 import bcrypt from "bcryptjs";
-import { signToken } from "@/lib/auth";
+import { signToken } from "../../../../lib/auth";
 import { cookies } from "next/headers";
 
 export async function POST(req) {
   await connectDB();
   const { email, password } = await req.json();
+
   const user = await User.findOne({ email });
   if (!user) return Response.json({ error: "No user" });
 
@@ -14,6 +15,7 @@ export async function POST(req) {
   if (!valid) return Response.json({ error: "Wrong password" });
 
   const token = signToken(user);
-  cookies().set("token", token);
+  cookies().set("token", token, { httpOnly: true });
+
   return Response.json({ success: true });
 }
